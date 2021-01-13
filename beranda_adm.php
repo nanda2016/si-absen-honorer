8dd7a527b3e7e14a593d5bb533313189a2acaac3
nanda2016 <77376148+nanda2016@users.noreply.github.com>
2021-01-13 13:05:04 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Absensi Tenaga Honorer Dinas Pendidikan Asahan</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/logo_dinas.png"></img></p>
+                          <p class="label-title" align="center"><strong>Dinas Pendidikan Kabupaten Asahan</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Absensi Tenaga Honorer Dinas Pendidikan Asahan.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data pegawai, dan pengelolaan data absensi pegawai honorer. <br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data pegawai dan data absensi pegawai.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead>
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
